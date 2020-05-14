4b2aeeea687eaefe04a4f23b2af0807bf43bdac2
CNLHB <1348844909@qq.com>
2020-05-15 02:45:04 +0800

    项目初始化完成，配置ts-antd-less-别名等，完成页面布局，侧边两列式布局。页面横向空间有限时，侧边导航可收起

---
+++ src/components/index/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "antd";
+
+const Index: React.FC<{ bg: string }> = (props: any) => (
+  <div className={props.bg}></div>
+);
+
+export default Index;
